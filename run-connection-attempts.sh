1853dead1d95bac55f071af2daf62a0d2257ad41
Emile Snyder <emile@circleci.com>
2014-07-25 11:44:59 -0700

    Cat 20K of data via ssh

--- run-connection-attempts.sh
+++ run-connection-attempts.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# verify simple connection
 ssh $SSH_USER@$SSH_HOST date
+
+# try running something that will send a fair bit of data
+ssh -v -v $SSH_USER@$SSH_HOST dd if=/dev/urandom bs=20480 count=1 | hexdump
